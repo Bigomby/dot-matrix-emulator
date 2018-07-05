cd3a3d925872115338de766b138b6f87d66b9d6c
Diego Fernández Barrera <bigomby@gmail.com>
2018-07-05 19:07:03 +0200

    Initial commit

---
+++ src/components/Matrix.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="container">
+    <header>
+      <h1 class="title">LED Matrix emulator</h1>
+    </header>
+    <article class="wrapper">
+      <div class="matrix">
+        <div>
+          <square :input="squares[0]" />
+          <square :input="squares[1]" />
+          <square :input="squares[2]" />
+          <square :input="squares[3]" />
+        </div>
+        <div>
+          <square :input="squares[4]" />
+          <square :input="squares[5]" />
+          <square :input="squares[6]" />
+          <square :input="squares[7]" />
+        </div>
+        <div>
+          <square :input="squares[8]" />
+          <square :input="squares[9]" />
+          <square :input="squares[10]" />
+          <square :input="squares[11]" />
+        </div>
+        <div>
+          <square :input="squares[12]" />
+          <square :input="squares[13]" />
+          <square :input="squares[14]" />
+          <square :input="squares[15]" />
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+import Square from './Square';
+import _ from 'lodash';
+
+import C from '../assets/characters';
+
+Array.range = function(n) {
+  return Array.apply(null, Array(n)).map((x, i) => i);
+};
+
+Object.defineProperty(Array.prototype, 'chunk', {
+  value: function(n) {
+    return Array.range(Math.ceil(this.length / n)).map((x, i) =>
+      this.slice(i * n, i * n + n),
+    );
+  },
+});
+
+function text2dots(text) {
+  const testArray = text.split('');
+
+  const dots = _.flatten(
+    testArray.map(char => C[char].dots.slice(0, C[char].width + 1)),
+  );
+
+  for (let i = 0; i < dots.length % 8; i++) {
+    dots.push(0x00);
+  }
+
+  return dots;
+}
+
+export default {
+  components: {
+    Square,
+  },
+
+  computed: {
+    text() {
+      return this.$route.query.text;
+    },
+  },
+
+  data() {
+    return {
+      squares: Array.from({ length: 16 }, () =>
+        Array.from({ length: 8 }, () => 0x00),
+      ),
+    };
+  },
+
+  mounted() {
+    console.log(this.text);
+
+    if (!this.text) {
+      return;
+    }
+    const text = text2dots(this.text);
+
+    text.chunk(8).forEach((square, i) => {
+      this.$set(this.squares, i, square);
+    });
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: sans-serif;
+}
+
+.container {
+  background-color: whitesmoke;
+  height: 100vh;
+}
+
+header {
+  display: flex;
+}
+
+.title {
+  color: dimgray;
+  margin: auto;
+  padding: 1.25em;
+}
+
+.wrapper {
+  display: flex;
+}
+
+.matrix {
+  background-color: #202020;
+  filter: drop-shadow(0 0 1em black);
+  margin: auto;
+}
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="square">
+    <div v-for="dot in square" :class="[ dot > 0 ? 'dot-on' : 'dot-off' ]" />
+  </div>
+</template>
+
+<script>
+import _ from 'lodash';
+
+const SQUARE_WIDTH = 8;
+const SQUARE_HEIGHT = 8;
+
+export default {
+  props: {
+    input: {
+      type: Array,
+      default: () => [0, 0, 0, 0, 0, 0, 0, 0],
+    },
+  },
+
+  computed: {
+    square() {
+      const square = this.input
+        .map(column => column.toString(2))
+        .map(column => column.padStart(8, '0'))
+        .map(column => column.split(''));
+
+      return _.flatten(_.unzip(square));
+    },
+  },
+
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.square {
+  transform: scaleY(-1);
+  border: 1px solid dimgray;
+  display: inline-grid;
+  width: 12em;
+  height: 12em;
+  grid-template-columns: repeat(8, 1fr);
+  align-items: center;
+  grid-column-gap: 0.5em;
+  grid-row-gap: 0.5em;
+  padding: 0.25em;
+}
+
+.dot-on {
+  background-color: red;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+
+.dot-off {
+  background-color: dimgray;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+</style>
